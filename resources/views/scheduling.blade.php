6b8139dc07973d1acf07c63e4e08cc04b3dc4244
jarriz26 <jarrizcasin@gmail.com>
2022-04-11 19:46:20 +0800

    Updating the form
    Update the size and padding of buttons and Creating table headers for schedule list

--- resources/views/scheduling.blade.php
+++ resources/views/scheduling.blade.php
@@ -7,16 +7,30 @@
 
     <form action = "" method = "post">
 
-        <div class = "d-flex p-3 " >
+        <div class = "d-flex p-3 justify-content-center" >
             <input type="submit" value="Add new Schedule" class="btn bg-3 text-white w-20">
                 <div class = "input-group input-group-lg w-50 px-3">
                     <input type="text" class="form-control  " placeholder="Search">
+                    <input type="submit" value="Search" class="btn bg-3 mx-3 text-white w-20">   
                 </div>
-
-                <div class = "px-2">
-                     <input type="submit" value="Search" class="btn bg-3 text-white w-20">
-                </div>       
+                      
         </div>
+
+        <table class = "table table-bordered">
+            <thead>
+                <tr>
+                    <th class = "text-center">Sunday</th>
+                    <th class = "text-center">Monday</th>
+                    <th class = "text-center">Tuesday</th>
+                    <th class = "text-center">Wednesday</th>
+                    <th class = "text-center">Thursday</th>
+                    <th class = "text-center">Friday</th>
+                    <th class = "text-center">Saturday</th>
+                </tr>
+            </thead>
+            
+        </table>
+
 
     
     </form>
