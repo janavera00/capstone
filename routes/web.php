4fd7570215e06e8300bdbd94d3323ac25bffc0b8
unknown <navera.johnarnulfo@gmail.com>
2022-05-22 16:52:56 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('login');
 });
+
 Route::get('/scheduling/{id}', function ($id) {
     return view('schedDetails');
-});
-Route::get('/home', function () {
-    return view('dashboard');
 });
 
 Route::get('/filing', [FilingController::class, 'show']);
